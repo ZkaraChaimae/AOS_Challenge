39b66c68c1f09bb1b076cebb81f90a6d6219bed4
Chaimae <zkara.chaimae@gmail.com>
2019-09-03 22:53:13 +0100

    Creating the form

--- loginapp/src/App.js
+++ loginapp/src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      psd: '',
+    };
+  }
+  formSubmitHandler (e) {    
+    console.dir("wowowow");
+    alert(this.state.email + this.state.psd);
+  }
+
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    let em = this.state.email;
+    alert(em);
+  }
+  myChangeHandler = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+    this.setState({[name]: value});
+  }
+  render() {
+    return (
+          <div className="App">
+            <header className="App-header">
+              <h1>Hello AOS</h1>
+      
+              <form onSubmit={this.mySubmitHandler}>
+                <input type="text" name="login" onChange={this.myChangeHandler.bind(this)} placeholder="Email"></input>
+                <br></br>
+                <input type="password" name="password" onChange={this.myChangeHandler.bind(this)} placeholder="Password"></input>
+                <br></br>
+                {/* <button onClick={this.formSubmitHandler.bind(this)}>Submit</button> */}
+                <input type='submit' />
+              </form>
+            </header>
+      
+            
+          </div>
+        );
+  }
 }
 
 export default App;
