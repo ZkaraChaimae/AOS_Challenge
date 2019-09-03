159edc31fc8a32eb23e969ea77b3f8055e5c74d2
Chaimae <zkara.chaimae@gmail.com>
2019-09-03 23:03:11 +0100

    Getting data from form : sucess

--- loginapp/src/App.js
+++ loginapp/src/App.js
@@ -3,47 +3,32 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      psd: '',
-    };
-  }
-  formSubmitHandler (e) {    
-    console.dir("wowowow");
-    alert(this.state.email + this.state.psd);
-  }
-
+  
   mySubmitHandler = (event) => {
     event.preventDefault();
-    let em = this.state.email;
-    alert(em);
+    const data = new FormData(event.target);
+    let email = data.get("login");
+    let psd = data.get("password");
+    alert(email + " " + psd);
   }
-  myChangeHandler = (event) => {
-    let name = event.target.name;
-    let value = event.target.value;
-    this.setState({[name]: value});
-  }
+
   render() {
     return (
-          <div className="App">
-            <header className="App-header">
-              <h1>Hello AOS</h1>
-      
-              <form onSubmit={this.mySubmitHandler}>
-                <input type="text" name="login" onChange={this.myChangeHandler.bind(this)} placeholder="Email"></input>
-                <br></br>
-                <input type="password" name="password" onChange={this.myChangeHandler.bind(this)} placeholder="Password"></input>
-                <br></br>
-                {/* <button onClick={this.formSubmitHandler.bind(this)}>Submit</button> */}
-                <input type='submit' />
-              </form>
-            </header>
-      
-            
-          </div>
-        );
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello AOS</h1>
+
+          <form onSubmit={this.mySubmitHandler}>
+            <input type="text" name="login" placeholder="Email"></input>
+            <br></br>
+            <input type="password" name="password" placeholder="Password"></input>
+            <br></br>
+            {/* <button onClick={this.formSubmitHandler.bind(this)}>Submit</button> */}
+            <input type='submit' />
+          </form>
+        </header>
+      </div>
+    );
   }
 }
 
